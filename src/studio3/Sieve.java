8c85a40fba92cb4a519594cd16bfa951f77fbc9b
hkaiyu1113 <122923883+hkaiyu1113@users.noreply.github.com>
2023-02-09 15:25:47 -0600

    Done

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -13,12 +13,6 @@ public class Sieve {
 		System.out.println("What's the maximum number? (n inclusive)");
 		int n = in.nextInt();
 		
-		//int[] numbers = new int[n-1];
-		
-		//for (int i = 0; i < (n-1); i++) 
-		//	{
-		//	numbers[i] = i+2;
-		//	};
 		boolean[] isComposite = new boolean[n-1];
 		
 		while (sieveAdd<=n)
